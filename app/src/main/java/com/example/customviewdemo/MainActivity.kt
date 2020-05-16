6b94e39b77de5ac297bd0f29f434821ac6aad83c
Orit Malki <orit@hero-apps.io>
2020-05-16 22:19:08 +0300

    revered to saved

--- app/src/main/java/com/example/customviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/customviewdemo/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.customviewdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PianoView.IKeyboardListener {
+
+    private var toggle: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(PianoView(this))
+        setContentView(R.layout.activity_main)
+
+        keyboard_view.listener = this
+
+        toggle_theme_btn.setOnClickListener {
+            keyboard_view.apply {
+                whiteKeysColor = ContextCompat.getColor(this@MainActivity, if (toggle) R.color.colorAccent else R.color.white)
+                blackKeysColor = ContextCompat.getColor(this@MainActivity, if (toggle) R.color.yellow else R.color.black)
+                toggle = !toggle
+            }
+
+        }
+    }
+
+    override fun keyOn(key: PianoView.Key) {
+        monitor.text = key.name
+        monitor.setTextColor(ContextCompat.getColor(this, if (!key.isBlack) R.color.colorAccent else R.color.magenta))
+    }
+
+    override fun keyOff(key: PianoView.Key) {
+        monitor.text = ""
     }
 }
